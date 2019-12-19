15577f08a1f94838bb00f5ac3706dd621ffd215c
Ivan Chyr - Codete <ivan.chyr@codete.com>
2019-12-19 17:21:22 +0100

    moved project to folder. created simple server.

--- main.go
+++ main.go
@@ -1,34 +1,16 @@
 package main
 
 import (
-	r "github.com/dancannon/gorethink"
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	session, err := r.Connect(r.ConnectOpts{
-		Address:  "localhost:28015",
-		Database: "rtsupport",
-	})
-
-	if err != nil {
-		log.Panic(err.Error())
-	}
-
-	router := NewRouter(session)
-
-	router.Handle("channel add", addChannel)
-	router.Handle("channel subscribe", subscribeChannel)
-	router.Handle("channel unsubscribe", unsubscribeChannel)
-
-	router.Handle("user edit", editUser)
-	router.Handle("user subscribe", subscribeUser)
-	router.Handle("user unsubscribe", unsubscribeUser)
-
-	router.Handle("message add", addChannelMessage)
-	router.Handle("message subscribe", subscribeChannelMessage)
-	router.Handle("message unsubscribe", unsubscribeChannelMessage)
-	http.Handle("/", router)
+	// fmt.Println("Hello, Ivan")
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":4000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from go!")
 }
