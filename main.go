a2377a1e1d0c773de1ad5fa4c93c9e0c3ced4413
Ivan Chyr - Codete <ivan.chyr@codete.com>
2019-12-20 11:47:16 +0100

    added handling of subscribe and add channel events

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"time"
+
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
+
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+type Channel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -14,7 +27,6 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	// fmt.Println("Hello, Ivan")
 	http.HandleFunc("/", handler)
 	fmt.Println("Server starting")
 	http.ListenAndServe(":4000", nil)
@@ -22,7 +34,6 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, "Hello from go!")
 	fmt.Println(r.URL)
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -30,12 +41,47 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	for {
-		msgType, msg, _ := socket.ReadMessage()
-		fmt.Println(string(msg))
-		if err = socket.WriteMessage(msgType, msg); err != nil {
+		var inMessage Message
+		var outMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
 			fmt.Println(err)
-			return
+			break
 		}
-
+		switch inMessage.Name {
+		case "channel add":
+			err := addChannel(inMessage.Data)
+			if err != nil {
+				outMessage = Message{"error", err}
+				if err := socket.WriteJSON(outMessage); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+		case "channel subscribe":
+			{
+				go subscribeChannel(socket)
+			}
+		}
 	}
+}
+
+func addChannel(data interface{}) error {
+	var channel Channel
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return err
+	}
+	channel.Id = "1"
+	fmt.Printf("%#v\n", channel)
+	return nil
+}
+
+func subscribeChannel(socket *websocket.Conn) {
+	// TODO: rethinkDB Query / changefeed
+	for {
+		time.Sleep(time.Second * 1)
+		message := Message{"channel add", Channel{"1", "Software Support"}}
+		socket.WriteJSON(message)
+	}
+
 }
