8e1ef7a11aafa3b6f8863f790b451602e16f1e2b
Ivan Chyr - Codete <ivan.chyr@codete.com>
2019-12-27 22:18:57 +0200

    flawed design with goroutine subscription

--- handlers.go
+++ handlers.go
@@ -19,3 +19,23 @@ func addChannel(client *Client, data interface{}) {
 		}
 	}()
 }
+
+func subscribeChannel(client *Client, data interface{}) {
+	// we know subscription to RethinkDB change feed  is blocking operation
+	// so we spawn separate goroutine for it
+	go func() {
+		cursor, err := r.Table("channels").
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			if change.NewValue != nil && change.OldValue == nil {
+				client.send <- Message{"channel add", change.NewValue}
+			}
+		}
+	}()
+}
